dce6da90389959ac72888a90c31c658ab76b3460
Dayman Novaes <daymannovaes@gmail.com>
2016-04-07 09:05:15 -0300

    images

--- gulp/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
-const tasks = fs.readdirSync('./gulp/tasks/');
-
-tasks.forEach((task) => {
-  require(path.join(__dirname, 'tasks', task));
-});
